162fe717d65b022f7fc1c90db1a93a1cdb05248d
Keith Lee <khlee930121@gmail.com>
2023-08-28 14:43:57 -0400

    test all endpoints

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 
 class Post(models.Model):
@@ -10,15 +11,26 @@ class Post(models.Model):
     description = models.CharField()
     gender = models.CharField()
     owner_id = models.IntegerField()
+    created_at = models.DateField(default=datetime.date.today)
+
+    def __str__(self):
+        return self.name
 
 
 class Comment(models.Model):
     text = models.CharField()
-    date_created = models.DateField()
     user_id = models.IntegerField()
     post_id = models.IntegerField()
+    updated_at = models.DateField(default=datetime.date.today)
+    created_at = models.DateField(default=datetime.date.today)
+
+    def __str__(self):
+        return self.text
 
 
 class Like(models.Model):
     user_id = models.CharField()
     post_id = models.CharField()
+
+    def __str__(self):
+        return 'Like'
