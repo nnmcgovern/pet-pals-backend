26de8f443cdabbb8e2c6121b17b3c0fa69723ad6
Diego Sousa <khesanh@Diegos-MacBook-Pro.local>
2023-08-28 15:57:44 -0400

    User Login

--- users/views.py
+++ users/views.py
@@ -6,12 +6,19 @@ from .serializers import UserSerializer
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
-
+from django.shortcuts import get_object_or_404
 
 @api_view(['POST'])
 def login(request):
+    user = get_object_or_404(User, username=request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response({'message': 'Invalid Login.'}, status=status.HTTP_400_BAD_REQUEST)
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(user)
     return Response({
-        'message': '로그인 되었습니다.'
+        'token': token.key,
+        'user': serializer.data,
+
     })
 
 @api_view(['POST'])
