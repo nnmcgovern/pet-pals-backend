e1a6b8890878479cc7936e2c286c1c61e85dd1cd
Diego Sousa <khesanh@Diegos-MacBook-Pro.local>
2023-08-28 16:22:42 -0400

    Check for user token

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.response import Response
 
 
@@ -37,9 +39,12 @@ def sign_up(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
+@authentication_classes([TokenAuthentication, SessionAuthentication])
+@permission_classes([IsAuthenticated])
 def get_token(request):
+    print(request)
     return Response({
-        'message': '로그인 되었습니다.'
+        'message': '토큰이 유효합니다.'
     })
 
 
